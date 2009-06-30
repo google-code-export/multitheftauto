4fe1c6300a559eb0b3f85d5d1eb81682900bf88c
ccw808 <ccw808@915fb2f0-b7ca-11dd-a720-8bda5143708b>
2009-06-30 09:13:25 +0000

    Created damage-anim branch

---
+++ branches/damage-anim/MTA10/core/CVideoModeManager.cpp
@@ -0,0 +1,366 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        core/CVideoModeManager.cpp
+*  PURPOSE:     Video mode related operations
+*  DEVELOPERS:  ccw <chris@codewave.co.uk>
+*
+*  Multi Theft Auto is available from http://www.multitheftauto.com/
+*
+*****************************************************************************/
+
+#include "StdInc.h"
+#include <game/CGame.h>
+
+using std::string;
+
+//////////////////////////////////////////////////////////
+//
+// Helper stuff
+//
+
+#define ZERO_ON_NEW \
+    void* operator new ( size_t size )              { void* ptr = ::operator new(size); memset(ptr,0,size); return ptr; } \
+    void* operator new ( size_t size, void* where ) { memset(where,0,size); return where; }
+
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager class
+//
+///////////////////////////////////////////////////////////////
+class CVideoModeManager : public CVideoModeManagerInterface
+{
+public:
+    ZERO_ON_NEW
+                        CVideoModeManager           ( void );
+                        ~CVideoModeManager          ( void );
+
+    // CVideoModeManagerInterface methods
+    virtual void        PreCreateDevice             ( D3DPRESENT_PARAMETERS* pp );
+    virtual void        PostCreateDevice            ( IDirect3DDevice9* pD3DDevice, D3DPRESENT_PARAMETERS* pp );
+    virtual void        PreReset                    ( D3DPRESENT_PARAMETERS* pp );
+    virtual void        PostReset                   ( D3DPRESENT_PARAMETERS* pp );
+    virtual void        GetNextVideoMode            ( int& iOutNextVideoMode, bool& bOutNextWindowed );
+    virtual bool        SetVideoMode                ( int nextVideoMode, bool bNextWindowed );
+    virtual bool        IsWindowed                  ( void );
+    virtual bool        IsMultiMonitor              ( void );
+
+private:
+    void                LoadCVars                   ( void );
+    void                SaveCVars                   ( void );
+
+    int                 m_bForceFullScreenOnce;
+    int                 m_bForceWindowed;
+    int                 m_ulForceBackBufferWidth;
+    int                 m_ulForceBackBufferHeight;
+    HWND                m_hDeviceWindow;
+    CGameSettings *     m_pGameSettings;
+    unsigned long       m_ulMonitorCount;
+
+    int                 m_iCurrentVideoMode;    // VideoMode this run
+    bool                m_bCurrentWindowed;
+    int                 m_iNextVideoMode;       // VideoMode next run
+    bool                m_bNextWindowed;
+};
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager instantiation
+//
+///////////////////////////////////////////////////////////////
+CVideoModeManagerInterface* NewVideoModeManager ( void )
+{
+    return new CVideoModeManager ();
+}
+
+CVideoModeManagerInterface* g_pVideoModeManager = NULL;
+
+CVideoModeManagerInterface* GetVideoModeManager ( void )
+{
+    if ( !g_pVideoModeManager )
+        g_pVideoModeManager = NewVideoModeManager ();
+    return g_pVideoModeManager;
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager implementation
+//
+///////////////////////////////////////////////////////////////
+CVideoModeManager::CVideoModeManager ( void )
+{
+    m_pGameSettings = CCore::GetSingleton ( ).GetGame ( )->GetSettings();
+    m_iCurrentVideoMode = 1;
+    m_bCurrentWindowed = false;
+    m_iNextVideoMode = 1;
+    m_bNextWindowed = false;
+}
+
+
+CVideoModeManager::~CVideoModeManager ( void )
+{
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::PreCreateDevice
+//
+//
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::PreCreateDevice ( D3DPRESENT_PARAMETERS* pp )
+{
+    m_hDeviceWindow = pp->hDeviceWindow;
+
+    // Load settings
+    LoadCVars ();
+
+    // Prime save values
+    m_iNextVideoMode = m_iCurrentVideoMode;
+    m_bNextWindowed  = m_bCurrentWindowed;
+
+    // Do windowed
+    if ( m_bCurrentWindowed )
+    {
+        int x, y;
+        x = GetSystemMetrics ( SM_CXSCREEN );
+        y = GetSystemMetrics ( SM_CYSCREEN );
+        SetWindowLong ( m_hDeviceWindow, GWL_STYLE, WS_POPUP );
+        MoveWindow ( m_hDeviceWindow, 
+                    (x/2)-(pp->BackBufferWidth/2), 
+                    (y/2)-(pp->BackBufferHeight/2), 
+                    pp->BackBufferWidth,
+                    pp->BackBufferHeight,
+                    TRUE );
+        pp->Windowed = true;
+        pp->FullScreen_RefreshRateInHz = 0;
+    }
+
+
+    // Final bits
+    if ( m_bCurrentWindowed )
+    {
+        m_bForceFullScreenOnce = false;
+        m_bForceWindowed = true;
+        m_ulForceBackBufferWidth  = pp->BackBufferWidth;
+        m_ulForceBackBufferHeight = pp->BackBufferHeight;
+    }
+    else
+    {
+        m_bForceFullScreenOnce = true;
+        m_bForceWindowed = false;
+        m_ulForceBackBufferWidth  = pp->BackBufferWidth;
+        m_ulForceBackBufferHeight = pp->BackBufferHeight;
+    }
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::PostCreateDevice
+//
+//
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::PostCreateDevice ( IDirect3DDevice9* pD3DDevice, D3DPRESENT_PARAMETERS* pp )
+{
+    // Make us windowed
+    if ( m_bCurrentWindowed )
+        pD3DDevice->Reset ( pp );
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::PreReset
+//
+//
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::PreReset ( D3DPRESENT_PARAMETERS* pp )
+{
+    if ( m_bForceFullScreenOnce )
+    {
+        m_bForceFullScreenOnce = false;
+        pp->Windowed = false;
+        pp->FullScreen_RefreshRateInHz = D3DPRESENT_RATE_DEFAULT;
+    }
+    else
+    if ( m_bForceWindowed )
+    {
+        pp->Windowed = true;
+        pp->FullScreen_RefreshRateInHz = 0;
+    }
+
+    pp->BackBufferWidth = m_ulForceBackBufferWidth;
+    pp->BackBufferHeight = m_ulForceBackBufferHeight;
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::PostReset
+//
+//
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::PostReset ( D3DPRESENT_PARAMETERS* pp )
+{
+    if ( pp->Windowed )
+    {
+        // Add frame
+        LONG Style = WS_VISIBLE | WS_CLIPSIBLINGS | WS_BORDER | WS_DLGFRAME | WS_SYSMENU;
+        SetWindowLong ( m_hDeviceWindow, GWL_STYLE, Style );
+
+        //LONG ExStyle = WS_EX_WINDOWEDGE;
+        //SetWindowLong ( m_hDeviceWindow, GWL_EXSTYLE, ExStyle );
+
+        // Ensure client area of window is correct size
+        RECT ClientRect = { 0, 0, pp->BackBufferWidth, pp->BackBufferHeight };
+        AdjustWindowRect( &ClientRect, GetWindowLong(m_hDeviceWindow,GWL_STYLE), FALSE );
+
+        int SizeX = ClientRect.right - ClientRect.left;
+        int SizeY = ClientRect.bottom - ClientRect.top;
+
+        SetWindowPos( m_hDeviceWindow, HWND_NOTOPMOST, 0, 0, SizeX, SizeY, SWP_NOMOVE | SWP_FRAMECHANGED );
+    }
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::GetNextVideoMode
+//
+//
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::GetNextVideoMode ( int& iOutNextVideoMode, bool& bOutNextWindowed )
+{
+    iOutNextVideoMode   = m_iNextVideoMode;
+    bOutNextWindowed    = m_bNextWindowed;
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::SetVideoMode
+//
+// Returns true if restart is required
+//
+///////////////////////////////////////////////////////////////
+bool CVideoModeManager::SetVideoMode ( int iNextVideoMode, bool bNextWindowed )
+{
+    bool bRequiresRestart = false;
+
+    // Resolution
+    if ( iNextVideoMode > 0 && iNextVideoMode < (int)m_pGameSettings->GetNumVideoModes () )
+    {
+        if ( m_iNextVideoMode != iNextVideoMode )
+        {
+            m_iNextVideoMode = iNextVideoMode;
+            if ( m_iCurrentVideoMode != iNextVideoMode )
+            {
+                bRequiresRestart = true;
+            }
+        }
+    }
+
+    // Windowed
+    if ( m_bNextWindowed != bNextWindowed )
+    {
+        m_bNextWindowed = bNextWindowed;
+        if ( m_bCurrentWindowed != bNextWindowed )
+        {
+            bRequiresRestart = true;
+        }
+    }
+
+    SaveCVars ();
+
+    return bRequiresRestart;
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::LoadCVars
+//
+// Loads to current
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::LoadCVars ( void )
+{
+    m_iCurrentVideoMode = m_pGameSettings->GetCurrentVideoMode ();
+    CVARS_GET ( "display_windowed",     m_bCurrentWindowed );
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::SaveCVars
+//
+// Saves from next
+//
+///////////////////////////////////////////////////////////////
+void CVideoModeManager::SaveCVars ( void )
+{
+    m_pGameSettings->SetCurrentVideoMode ( m_iNextVideoMode, true );
+    CVARS_SET ( "display_windowed",     m_bNextWindowed );
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::IsWindowed
+//
+//
+//
+///////////////////////////////////////////////////////////////
+bool CVideoModeManager::IsWindowed ( void )
+{
+    return m_bCurrentWindowed;
+}
+
+
+///////////////////////////////////////////////////////////////
+//
+// CVideoModeManager::IsMultiMonitor
+//
+//
+//
+///////////////////////////////////////////////////////////////
+bool CVideoModeManager::IsMultiMonitor ( void )
+{
+    if ( m_ulMonitorCount == 0 )
+    {
+        // Count attached monitors
+        for ( int i = 0 ; true ; i++ )
+        {
+            DISPLAY_DEVICE device;
+            device.cb = sizeof(device);
+
+            // Get next DISPLAY_DEVICE from the system
+            if( !EnumDisplayDevicesA ( NULL, i, &device, 0 ) )
+                break;
+
+            // Calc flags
+            bool bAttachedToDesktop = ( device.StateFlags & DISPLAY_DEVICE_ATTACHED_TO_DESKTOP ) != 0;
+            bool bMirroringDriver   = ( device.StateFlags & DISPLAY_DEVICE_MIRRORING_DRIVER ) != 0;
+            bool bPrimaryDevice     = ( device.StateFlags & DISPLAY_DEVICE_PRIMARY_DEVICE ) != 0;
+
+            // Ignore devices that are not required
+            if( !bAttachedToDesktop || bMirroringDriver )
+                continue;
+
+            m_ulMonitorCount++;
+        }
+    }
+
+    return m_ulMonitorCount > 1;
+}
---
+++ branches/damage-anim/MTA10/mods/shared_logic/CClientExplosionManager.cpp
@@ -0,0 +1,187 @@
+/*****************************************************************************
+*
+*  PROJECT:     Multi Theft Auto v1.0
+*               (Shared logic for modifications)
+*  LICENSE:     See LICENSE in the top level directory
+*  FILE:        mods/shared_logic/CClientExplosionManager.cpp
+*  PURPOSE:     Explosion event manager class
+*  DEVELOPERS:  Jax <>
+*
+*****************************************************************************/
+
+#include <StdInc.h>
+
+extern CClientGame* g_pClientGame;
+
+CClientExplosionManager * g_pExplosionManager = NULL;
+
+CClientExplosionManager::CClientExplosionManager ( CClientManager * pManager )
+{
+    g_pExplosionManager = this;
+    m_pManager = pManager;
+    m_LastWeaponType = WEAPONTYPE_UNARMED;
+    m_pLastCreator = NULL;
+}
+
+
+CClientExplosionManager::~CClientExplosionManager ( void )
+{
+    if ( g_pExplosionManager == this ) g_pExplosionManager = NULL;
+}
+
+
+bool CClientExplosionManager::Hook_StaticExplosionCreation ( CEntity* pGameExplodingEntity, CEntity* pGameCreator, const CVector& vecPosition, eExplosionType explosionType )
+{
+    return g_pExplosionManager->Hook_ExplosionCreation ( pGameExplodingEntity, pGameCreator, vecPosition, explosionType );
+}
+
+
+bool CClientExplosionManager::Hook_ExplosionCreation ( CEntity* pGameExplodingEntity, CEntity* pGameCreator, const CVector& vecPosition, eExplosionType explosionType )
+{
+    CClientPlayer * pLocalPlayer = m_pManager->GetPlayerManager ()->GetLocalPlayer ();
+
+    // Grab the entity responsible
+    CClientEntity * pResponsible = NULL;
+    CEntity* pResponsibleGameEntity = ( pGameExplodingEntity ) ? pGameExplodingEntity : pGameCreator;
+    if ( pResponsibleGameEntity )
+        pResponsible = m_pManager->FindEntity ( pResponsibleGameEntity, false );
+
+    eWeaponType explosionWeaponType;
+    switch ( explosionType )
+    {
+        case EXP_TYPE_GRENADE:
+        {
+            // Grenade type explosions from vehicles should only be freefall bombs
+            // TODO: need a way to check if its a freefall bomb if creator is a ped
+            if ( pGameCreator && pGameCreator->GetEntityType () == ENTITY_TYPE_VEHICLE )
+                explosionWeaponType = WEAPONTYPE_FREEFALL_BOMB;
+            else
+                explosionWeaponType = WEAPONTYPE_GRENADE;
+            break;
+        }
+        case EXP_TYPE_MOLOTOV:
+            explosionWeaponType = WEAPONTYPE_MOLOTOV;
+            break;
+        case EXP_TYPE_ROCKET:
+        case EXP_TYPE_ROCKET_WEAK:
+            explosionWeaponType = WEAPONTYPE_ROCKET;
+            break;
+        case EXP_TYPE_TANK_GRENADE:
+            explosionWeaponType = WEAPONTYPE_TANK_GRENADE;
+            break;
+        default:
+            explosionWeaponType = WEAPONTYPE_EXPLOSION;
+            break;
+    }
+
+    // Got a responsible entity?
+    if ( pResponsible )
+    {
+        // Is the local player responsible for this?
+        bool bLocal = ( ( pResponsible == pLocalPlayer ) ||
+                      ( pResponsible == pLocalPlayer->GetOccupiedVehicle () ) ||
+                      ( g_pClientGame->GetUnoccupiedVehicleSync ()->Exists ( static_cast < CDeathmatchVehicle * > ( pResponsible ) ) ) );
+
+        if ( bLocal )
+        {
+            CClientEntity * pOriginSource = NULL;
+
+            // Is this an exploding vehicle?
+            if ( pGameExplodingEntity && pGameExplodingEntity->GetEntityType () == ENTITY_TYPE_VEHICLE )
+            {
+                // Set our origin-source to the vehicle
+                pOriginSource = m_pManager->FindEntity ( pGameExplodingEntity, false );
+            }                
+            // If theres other players, sync it relative to the closest (lag compensation)
+            else if ( m_pManager->GetPlayerManager ()->Count () > 1 )
+            {
+                switch ( explosionWeaponType )
+                {
+                    case WEAPONTYPE_ROCKET:
+                    case WEAPONTYPE_ROCKET_HS:
+                    {
+                        CClientPlayer * pPlayer = g_pClientGame->GetClosestRemotePlayer ( vecPosition, 200.0f );
+                        if ( pPlayer )
+                        {
+                            pOriginSource = pPlayer;
+                        }
+                        break;
+                    }
+                }                                   
+            }
+            // Request a new explosion
+            g_pClientGame->SendExplosionSync ( vecPosition, explosionType, pOriginSource );
+
+            // If caused by the local player in a fast vehicle then play it locally, and ignore when the server sends it back
+            if ( pLocalPlayer && pLocalPlayer->GetOccupiedVehicle () )
+            {
+                CVector vecVelocity;
+                pLocalPlayer->GetOccupiedVehicle ()->GetMoveSpeedMeters ( vecVelocity );
+
+                // Check vehicle speed
+                if ( vecVelocity.Length () > 10 )
+                {
+                    // Remember what the explosion looks like, so we can ignore it when the server sends it back
+                    SIgnoreItem ignoreItem;
+                    ignoreItem.fExpireTime   = GetSecondCount () + 2.0f;
+                    ignoreItem.vecPosition   = vecPosition;
+                    ignoreItem.explosionType = explosionType;
+                    m_IgnoreList.push_back( ignoreItem );
+
+                    // Play it locally
+                    return true;
+                }
+            }
+        }
+    }
+
+    // All explosions are handled server side
+    return false;
+}
+
+
+CExplosion * CClientExplosionManager::Create ( eExplosionType explosionType, CVector & vecPosition, CClientEntity * pCreator, bool bMakeSound, float fCamShake, bool bNoDamage, eWeaponType responsibleWeapon )
+{
+    // See if this explosion should be ignored
+    std::vector < SIgnoreItem >::iterator iter = m_IgnoreList.begin ();
+    while ( iter != m_IgnoreList.end () )
+    {
+        // Found match?
+        if ( iter->vecPosition == vecPosition && iter->explosionType == explosionType )
+        {
+            iter = m_IgnoreList.erase ( iter );
+            return NULL;
+        }
+
+        // Item too old ?
+        if ( iter->fExpireTime < GetSecondCount () )
+        {
+            iter = m_IgnoreList.erase ( iter );
+            continue;
+        }
+        iter++;
+    }
+    
+
+    CEntity * pGameCreator = NULL;
+    if ( pCreator ) pGameCreator = pCreator->GetGameEntity ();
+
+    // Update our records first?
+    m_pLastCreator = pCreator;
+    if ( responsibleWeapon != WEAPONTYPE_UNARMED ) m_LastWeaponType = responsibleWeapon;
+    else
+    {
+        switch ( explosionType )
+        {
+            case EXP_TYPE_GRENADE: m_LastWeaponType = WEAPONTYPE_GRENADE; break;
+            case EXP_TYPE_MOLOTOV: m_LastWeaponType = WEAPONTYPE_MOLOTOV; break;
+            case EXP_TYPE_ROCKET:
+            case EXP_TYPE_ROCKET_WEAK: m_LastWeaponType = WEAPONTYPE_ROCKET; break;
+            case EXP_TYPE_TANK_GRENADE: m_LastWeaponType = WEAPONTYPE_TANK_GRENADE; break;
+            default: m_LastWeaponType = WEAPONTYPE_EXPLOSION; break;
+        }
+    }
+
+    CExplosion * pExplosion = g_pGame->GetExplosionManager ()->AddExplosion ( explosionType, &vecPosition, pGameCreator, bMakeSound, fCamShake, bNoDamage );
+    return pExplosion;
+}
